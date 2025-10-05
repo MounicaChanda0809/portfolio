c62c4e917965937a7f26b11158fa8a2151ebaaa4
Naveen0197 <naveenreddystyle@gmail.com>
2025-10-05 18:47:37 -0500

    jirastory: 101_ updated full name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Naveen Reddy Googula
+          Learn React Naveen Reddy Googula.
         </a>
       </header>
     </div>
