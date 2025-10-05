79c9441757b0afb99beff477df856bf814948632
Naveen0197 <naveenreddystyle@gmail.com>
2025-10-05 18:45:39 -0500

    jirastory :101

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Naveen2
+          Learn React Naveen Reddy Googula
         </a>
       </header>
     </div>
