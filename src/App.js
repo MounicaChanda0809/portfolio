ce91f2fb016cfb1dbf447303016bd3515cc3b0c5
Naveen0197 <naveenreddystyle@gmail.com>
2025-10-11 18:32:48 -0500

    new_userstory20810: added to react link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Naveen's git learning experience 
         </a>
       </header>
     </div>
